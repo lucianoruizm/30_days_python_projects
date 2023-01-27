cddf2b7c0f1904b3e6aea7fb74da53d1740ae9a6
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-01-26 21:20:34 -0300

    01

---
+++ 01_selenium_web_click/main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+
+driver = webdriver.Firefox()
+driver.get('https://www.anerbarrena.com/demos/2013/12-radio-button-jquery-demo2.php')
+
+# radio_btn = driver.find_element(By.PATH,'//input[@id="edad2"]')
+radio_btn = driver.find_element('xpath','//input[@id="edad2"]')
+radio_btn.click()
+
+send_btn = driver.find_element('id', 'boton')
+send_btn.click()
+
+alert = Alert(driver)
+alert.accept()
---
+++ README.md
@@ -0,0 +1,16 @@
+01- Ejemplo de automatización de navegador, clickeando en input de tipo radio button
+    Instalación de Selenium:
+      pip install -U selenium
+    Drivers:
+        Chrome:
+          https://chromedriver.chromium.org/downloads
+        
+        Edge:
+          https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+        
+        Firefox:
+          https://github.com/mozilla/geckodriver/releases
+        
+        Safari:
+          https://webkit.org/blog/6900/webdriver-support-in-safari-10/
+  
